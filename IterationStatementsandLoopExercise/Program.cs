5ad89b7c951d1dd987ff9e81aaa131a0c5b4b9db
Caprena Richie <robcaprena@gmail.com>
2021-10-04 12:44:37 -0400

    first commit

---
+++ IterationStatementsandLoopExercise/Program.cs
@@ -0,0 +1,162 @@
+﻿using System;
+
+namespace IterationStatementsandLoopExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Creating Loop to run 15 times
+            Console.WriteLine("Creating Loop");
+            for (int i = 0; i < 15; i++)
+            {
+                                      
+                Console.WriteLine((i));
+            }
+
+            Console.WriteLine();
+            Console.WriteLine ("---------");
+            Console.WriteLine();
+
+            Console.WriteLine("Creating Loop");
+            for (int i = 0; i < 15; i++)
+            {
+                Console.WriteLine((i));
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("---------");
+            Console.WriteLine();
+
+            Console.WriteLine("Creating Loop");
+            for (int i = 0; i < 15; i++)
+            {
+                Console.WriteLine((i));
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("---------");
+            Console.WriteLine();
+
+            Console.WriteLine("Creating Loop");
+            for (int i = 0; i < 15; i++)
+            {
+                Console.WriteLine((i));
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("---------");
+            Console.WriteLine();
+
+            Console.WriteLine("Creating Loop");
+            for (int i = 0; i < 15; i++)
+            {
+                Console.WriteLine((i));
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("---------");
+            Console.WriteLine();
+
+            Console.WriteLine("Creating Loop");
+            for (int i = 0; i < 15; i++)
+            {
+                Console.WriteLine((i));
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("---------");
+            Console.WriteLine();
+
+            Console.WriteLine("Creating Loop");
+            for (int i = 0; i < 15; i++)
+            {
+                Console.WriteLine((i));
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("---------");
+            Console.WriteLine();
+
+            Console.WriteLine("Creating Loop");
+            for (int i = 0; i < 15; i++)
+            {
+                Console.WriteLine((i));
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("---------");
+            Console.WriteLine();
+
+            Console.WriteLine("Creating Loop");
+            for (int i = 0; i < 15; i++)
+            {
+                Console.WriteLine((i));
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("---------");
+            Console.WriteLine();
+
+            Console.WriteLine("Creating Loop");
+            for (int i = 0; i < 15; i++)
+            {
+                Console.WriteLine((i));
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("---------");
+            Console.WriteLine();
+
+            Console.WriteLine("Creating Loop");
+            for (int i = 0; i < 15; i++)
+            {
+                Console.WriteLine((i));
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("---------");
+            Console.WriteLine();
+
+            Console.WriteLine("Creating Loop");
+            for (int i = 0; i < 15; i++)
+            {
+                Console.WriteLine((i));
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("---------");
+            Console.WriteLine();
+
+            Console.WriteLine("Creating Loop");
+            for (int i = 0; i < 15; i++)
+            {
+                Console.WriteLine((i));
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("---------");
+            Console.WriteLine();
+
+            Console.WriteLine("Creating Loop");
+            for (int i = 0; i < 15; i++)
+            {
+                Console.WriteLine((i));
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("---------");
+            Console.WriteLine();
+
+            Console.WriteLine("Creating Loop");
+            for (int i = 0; i < 15; i++)
+            {
+                Console.WriteLine((i));
+            }*/
+
+
+
+
+        }
+    }
+}
